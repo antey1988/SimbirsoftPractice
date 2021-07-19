e171ff3d39989b20fe4ee0054e40148a93a9ea42
OlegPetrov <antey1988@gmail.com>
2021-07-19 10:26:58 +0300

    added dependencies: web,jpa, swagger

---
+++ src/main/java/com/example/SimbirsoftPractice/SimbirsoftPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimbirsoftPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirsoftPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirsoftPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimbirsoftPractice/SimbirsoftPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimbirsoftPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirsoftPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
